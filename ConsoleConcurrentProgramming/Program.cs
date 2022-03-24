4d9e67392f3b3f908927bcd94e6a228443e68044
Tomasz Roske <236639@edu.p.lodz.pl>
2022-03-24 21:46:42 +0100

    add console to project

---
+++ ConsoleConcurrentProgramming/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+ConcurrentProgramming.FirstProgram firstProgram = new ConcurrentProgramming.FirstProgram(5, 6);
+Console.WriteLine(firstProgram.HelloWorld());
+Console.WriteLine($"{firstProgram.A}+{firstProgram.B}={firstProgram.Add()}");
