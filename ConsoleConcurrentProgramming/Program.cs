bebfd15ba3b948b52c98f5c7ef7a0f57f964cfd3
Tomasz Roske <73096522+Pevth@users.noreply.github.com>
2022-04-28 18:52:47 +0200

    Delete folder

--- ConsoleConcurrentProgramming/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-ConcurrentProgramming.FirstProgram firstProgram = new ConcurrentProgramming.FirstProgram(5, 6);
-Console.WriteLine(firstProgram.HelloWorld());
-Console.WriteLine($"{firstProgram.A}+{firstProgram.B}={firstProgram.Add()}");
