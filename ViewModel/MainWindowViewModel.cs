1c5056959ed93ff0efdef16b5ae0c56d4faf2f7c
Bartosz Siekański <102323194+siekaan@users.noreply.github.com>
2022-05-19 20:37:11 +0200

    Small View Changes

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,23 +1,22 @@
 ﻿using Model;
-using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Threading;
+using System;
+using System.Threading.Tasks;
 
 
 namespace ViewModel
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase  ///tu jak cos zmienic na inotify
     {
         private ModelAPI ModelAPI { get; set; } = ModelAPI.CreateApi();
         private string _numberOfBalls;
-        private Task updating;
 
-        public ObservableCollection<BallModel> Balls { get; set; }
+        public ObservableCollection<IBall> Balls { get; set; }
 
         public ICommand ButtonClick { get; set; }
 
@@ -35,32 +34,17 @@ namespace ViewModel
             }
         }
 
-        
-
         public MainWindowViewModel() : this(ModelAPI.CreateApi()) { }
 
         public MainWindowViewModel(ModelAPI modelAPI)
         {
             ModelAPI = modelAPI;
-            Balls = new ObservableCollection<BallModel>();
+            Balls = new ObservableCollection<IBall>();
             ButtonClick = new RelayCommand(() => ClickHandler());
+            IDisposable observer = ModelAPI.Subscribe(x => Balls.Add(x));
         }
 
-        public void UpdateBalls()
-        {
-            while (true)
-            {
-                ObservableCollection<BallModel> treadList = new ObservableCollection<BallModel>();
-                foreach (BallModel ball in ModelAPI.ListOfBallModel)
-                {
-                    treadList.Add(ball);
-                }
 
-                Balls = treadList;
-                RaisePropertyChanged(nameof(Balls));
-                Thread.Sleep(10);
-            }
-        }
 
         public int readNumberOfBalls()
         {
@@ -68,19 +52,14 @@ namespace ViewModel
             if (Int32.TryParse(numberOfBalls, out value))
             {
                 value = Int32.Parse(numberOfBalls);
-                return value; 
+                return value;
             }
             return 0;
         }
 
         private void ClickHandler()
         {
-            ModelAPI.createBalls(readNumberOfBalls());
-            ModelAPI.moveBalls();
-
-            updating = new Task(UpdateBalls);
-            updating.Start();
-
+            ModelAPI.OKLetsGo(readNumberOfBalls());
         }
 
     }
